455ffe523be3a081d0b2f494e97b08f70336b45c
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-10-17 17:11:24 -0400

    more form stuff

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,7 +25,8 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" netlify>
+          <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+            <input type="hidden" name="bot-field" />
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
