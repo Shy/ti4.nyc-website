a8809d2eba68d726b219acf72f312ce3c35993df
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-10-17 16:46:44 -0400

    Small typo

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,7 +25,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" netlifyh>
+          <form name="contact" method="POST" netlify>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
