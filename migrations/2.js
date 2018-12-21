fe901cb4efa4207aabe06245533e0ce06ec3905c
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-12-21 16:07:28 -0500

    removed migration info

--- migrations/2.js
+++
@@ -1,9 +0,0 @@
-module.exports = function runMigration(migration) {
-    const table = migration.editContentType("table");
-    table
-        .createField("winner")
-        .name("winner")
-        .type("Symbol")
-        .required(false);
-  return;
-};
--- migrations/3.js
+++
@@ -1,6 +0,0 @@
-module.exports = function runMigration(migration) {
-    const table = migration.editContentType("table");
-    table
-        .deleteField("winner");
-  return;
-};
