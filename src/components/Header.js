1dee3143b02225f743b5aef1b9459d95edc9c67e
Shy Ruparel <Shyamalruparel1991@gmail.com>
2019-01-03 21:54:36 -0500

    Added signup form

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,8 +19,9 @@ const Header = (props) => (
                 {props.cfData.map(trick => (
                     <li><a href="javascript:;" onClick={() => {props.onOpenArticle(trick.node.slug)}}>{trick.node.title}</a></li>
                 ))}
+                <li><a href="https://ti4nyc.typeform.com/to/Apbesg">Sign up for a Game</a></li>
+                <li><a href="https://www.facebook.com/groups/TI.NYC/">Join our FB Group</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('past-games')}}>Game Log</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
     </header>
--- src/components/Main.js
+++ src/components/Main.js
@@ -88,50 +88,7 @@ class Main extends React.Component {
           {close}
         </article>
 
-        <article
-          id="contact"
-          className={`${this.props.article === "contact" ? "active" : ""} ${
-            this.props.articleTimeout ? "timeout" : ""
-          }`}
-          style={{ display: "none" }}
-        >
-          <h2 className="major">Contact</h2>
-          <form
-            name="contact"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            <input type="hidden" name="bot-field" />
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4" />
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-            </ul>
-          </form>
-          <ul className="icons">
-            <li>
-              <a
-                href="https://www.facebook.com/groups/TI.NYC/"
-                className="icon fa-facebook"
-              >
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-          </ul>
-          {close}
-        </article>
+
       </div>
     );
   }
